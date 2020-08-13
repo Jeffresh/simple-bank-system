e86bebb3a8b7156fdccd9eaaa05be9aec49e7961
Jeff <jeff.uca@gmail.com>
2020-08-13 18:10:08 +0200

    :sparkles: add basic structure of the class with the necessary functions

--- simple_bank_system.py
+++ simple_bank_system.py
@@ -1,5 +1,38 @@
 class BankSystem:
-    pass
+    CARDS = ['Visa', 'American Express', 'Mastercard']
+    OPTIONS = [0, 1, 2]
+
+    def check_credit_card(self, card_number):
+        pass
+
+    @staticmethod
+    def main_menu():
+        pass
+
+    @staticmethod
+    def user_menu():
+        pass
+
+    @staticmethod
+    def create_account():
+        pass
+
+    @staticmethod
+    def login():
+        pass
+
+    @staticmethod
+    def get_balance():
+        pass
+
+    @staticmethod
+    def log_out():
+        pass
+
+    def start(self):
+        option = BankSystem.main_menu()
+        while option != 0 and option in BankSystem.OPTIONS:
+            option = BankSystem.main_menu()
 
 
 if __name__ == '__main__':
