0e59282a65dc3d58dbfba649128e2e33b7e3d953
Jeff <jeff.uca@gmail.com>
2020-08-13 18:47:14 +0200

    :sparkles: add create card number function

--- simple_bank_system.py
+++ simple_bank_system.py
@@ -1,3 +1,6 @@
+import random as rd
+
+
 class BankSystem:
     CARDS = ['Visa', 'American Express', 'Mastercard']
     OPTIONS = [0, 1, 2]
@@ -10,12 +13,22 @@ class BankSystem:
         pass
 
     @staticmethod
+    def create_card_number():
+        inn = '400000'
+        account_number = str(rd.randint(0, 9999999999))
+        card_number = inn + '0' * (10 - len(account_number)) + account_number
+        return card_number
+
+
+    @staticmethod
     def user_menu():
         pass
 
     @staticmethod
     def create_account():
-        pass
+        print('Your card has been created')
+        credit_card = BankSystem.create_credit_card()
+        print(credit_card)
 
     @staticmethod
     def login():
@@ -36,12 +49,14 @@ class BankSystem:
     def start(self):
         option = BankSystem.main_menu()
         while option != 0 and option in BankSystem.OPTIONS:
-            option = BankSystem.main_menu()
             if option in BankSystem.OPTIONS:
                 if option == 1:
                     BankSystem.create_account()
                 if option == 2:
                     BankSystem.login()
+
+            option = BankSystem.main_menu()
+
 
         if option not in BankSystem.OPTIONS:
             raise ValueError("Invalid option '{}' ".format(option))
