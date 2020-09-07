f085cd51dd7a5de6302d0c00eddbefea7b3e0391
Jeff <jeff.uca@gmail.com>
2020-09-07 10:47:52 +0200

    :sparkles: add basic database api and new methods to interact with the database

--- database/database.py
+++ database/database.py
@@ -9,11 +9,29 @@ class Database:
         number VARCHAR(16) NOT NULL UNIQUE, pin VARCHAR(4) NOT NULL , balance INT DEFAULT  0)''')
         self.conn.commit()
 
+    def __init__(self):
+        self.conn = sqlite3.connect('database/card.s3db')
+        self.create_card_table()
+
+
+class BankDatabaseApi:
+
     def add_card(self, card_number, card_pin):
         cur = self.conn.cursor()
         cur.execute(f'''INSERT INTO card(number, pin) VALUES({card_number}, {card_pin})''')
         self.conn.commit()
 
+    def remove_card(self, card_number):
+        pass
+
+    def mod_balance(self, card_number):
+        pass
+
+    def mod_pin(self, card_number, new_pin):
+        pass
+
+    def get_balance(self, card_number):
+        pass
+
     def __init__(self):
         self.conn = sqlite3.connect('database/card.s3db')
-        self.create_card_table()
